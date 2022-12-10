888981197a4b4e697706520db3df99ff5a6b3fcd
Michael Michaelou <71216390+MichaelMichael1997@users.noreply.github.com>
2022-12-10 17:33:42 +0200

    refactor: Cleanup/Deletions
    Cleanup/Deletions of useless functions

--- grpc/src/arax_grpc_client/arax_grpc_client.h
+++ grpc/src/arax_grpc_client/arax_grpc_client.h
@@ -30,76 +30,29 @@
 
 // -------------------- Arax Client Class --------------------
 
-class AraxClient{
+class AraxClient {
 private:
-  std::unique_ptr<arax::Arax::Stub> stub_; // Only member functions should have access to this
+    std::unique_ptr<arax::Arax::Stub> stub_; // Only member functions should have access to this
 
 public:
 
-  /*
-   * Constructors
-   * 
-   */
-  AraxClient(std::shared_ptr<grpc::Channel> channel);
-  
-  /*
-   * Destructors
-   */
-  ~AraxClient();
-  
-  // -------------------- Arax Client Services --------------------
-  
-  /*
-   * Initialize Arax
-   *
-   * This should be called by all applications prior
-   * to using any other Arax function.
-   *
-   * @return An arax_pipe_s c struct object
-   */
-   // void client_arax_init();
-   
-   /*
-    * Exit and cleanup Arax
-    */
-   // void client_arax_exit();
-   
-   /*
-    * Delete the shared segment
-    */
+    /*
+     * Constructors
+     *
+     */
+    AraxClient(std::shared_ptr<grpc::Channel> channel);
+
+    /*
+     * Destructors
+     */
+    ~AraxClient();
+
+    // -------------------- Arax Client Services --------------------
+
+    /*
+     * Delete the shared segment
+     */
     void client_arax_clean();
-
-  /*
-   * Returns the number of accelerators of a specific type
-   * TODO: Plus some other things
-   */
-    arax::AccelListResponse client_arax_accel_list(arax_accel_type_e type, bool physical);
-
-    /*
-     * Returns arax revision
-     *
-     * @return string with arax revision
-     */
-    const char* client_arax_pipe_revision();
-    
-    /*
-     * Increase process counter for arax_pipe
-     *
-     * @return Number of active processes before adding issuer
-     */
-    uint64_t client_arax_pipe_add_process();
-
-    /*
-     * Decrease process counter for pipe
-     *
-     * @return Number of active processes before removing issuer
-     */
-    uint64_t client_arax_pipe_del_process();
-
-    /*
-     * Free memory of accelerator array returned by arax accel list
-     */
-    void client_arax_accel_list_free();
 
     /*
      * Get the type of accelerator, specified in the AccelDescriptor request message
@@ -117,25 +70,25 @@ public:
      *
      * @return The ID for the newly allocated buffer, or 0 if failed
      */
-      uint64_t client_arax_buffer(size_t size);
+    uint64_t client_arax_buffer(size_t size);
 
     /*
      * Register a new process 'func_name'
      *
      * @param func_name The name of the process
      *
-     * @return The id of the arax_proc resource, or 0 on failure 
+     * @return The id of the arax_proc resource, or 0 on failure
      */
-      uint64_t client_arax_proc_register(const char* func_name);
+    uint64_t client_arax_proc_register(const char *func_name);
 
-     /*
-      * Delete registered arax proc pointer
-      *
-      * @param proc The name of the process 
-      *
-      * @return nothing
-      */
-      void client_arax_proc_put(uint64_t id);
+    /*
+     * Delete registered arax proc pointer
+     *
+     * @param proc The name of the process
+     *
+     * @return nothing
+     */
+    void client_arax_proc_put(uint64_t id);
 
     /*
      * Acquire a virtual accelerator of the given type
@@ -144,16 +97,16 @@ public:
      *
      * @return The id of the acquire resource or 0 on failure
      */
-      uint64_t client_arax_accel_acquire_type(unsigned int type);
+    uint64_t client_arax_accel_acquire_type(unsigned int type);
 
     /*
      * Release previously acquired accelerator
      *
-     * @param id The id of the accelerator 
+     * @param id The id of the accelerator
      *
-     * @return nothing 
+     * @return nothing
      */
-      void client_arax_accel_release(uint64_t id);
+    void client_arax_accel_release(uint64_t id);
 
     /*
      * Set data to buffer
@@ -162,10 +115,10 @@ public:
      * @param accel The ID of the accelerator
      * @param value The string value to be passed to buffer
      * TODO: Change this depending on the possible values a buffer can get
-     * 
-     * @return nothing 
+     *
+     * @return nothing
      */
-    void client_arax_set_data(uint64_t buffer, uint64_t accel, const char* value);
+    void client_arax_set_data(uint64_t buffer, uint64_t accel, const char *value);
 
     /*
      * Issue a new task
@@ -173,21 +126,21 @@ public:
      * @param accel The ID of the accelerator responsible for executing the task
      * @param proc ID of the arax_proc to be dispatched on accelerator
      * @param in_count Size of input array (elements)
-     * @param in_buffer Input buffer 
+     * @param in_buffer Input buffer
      * @param out_count Size of output array (elements)
-     * @param out_buffer Output buffer 
+     * @param out_buffer Output buffer
      *
      * @return The id of the new task or 0 on failure
      */
-     uint64_t client_arax_task_issue(uint64_t accel, uint64_t proc, size_t in_count, uint64_t in_buffer,
-                size_t out_count, uint64_t out_buffer);
+    uint64_t client_arax_task_issue(uint64_t accel, uint64_t proc, size_t in_count, uint64_t in_buffer,
+      size_t out_count, uint64_t out_buffer);
 
     /*
      * Decrease ref counter of task
      *
      * @param task The ID of the task
      *
-     * @return 
+     * @return
      */
     void client_arax_task_free(uint64_t task);
 
@@ -199,61 +152,7 @@ public:
      * @return The state of the task or -1 on failure
      */
     int client_arax_task_wait(uint64_t task);
-
-    /*
-     * Gen an orphan/Unassigned virtual accelerator of NULL
-     * Function will sleep if there are no orphans exist at the time of the call
-     *
-     * @return The ID of the retrieved vaccel or 0 on failure
-     */
-    uint64_t client_arax_pipe_get_orphan_vaccel();
-
-    /*
-     * Add vac with ID \c id to the list of orphan_vacs/unassigned accels
-     *
-     * @param id The ID of the vaccel
-     *
-     * @return nothing
-     */
-    void client_arax_pipe_add_orphan_vaccel(uint64_t id);
-
-    /*
-     * Returns false if there are no orphan vaccels
-     *
-     * @return true if orphan vaccels exist, false otherwise
-     */
-    bool client_arax_pipe_have_orphan_vaccels();
-
-    /*
-     * Remove vaccel, specified by \c id, from the orphan vaccel list
-     *
-     * @param id The id for the vaccel
-     *
-     * @return void
-     */
-    void client_arax_pipe_remove_orphan_vaccel(uint64_t id);
-
 }; /* class AraxClient */
 
 
 #endif /* ifndef ARAX_GRPC_CLIENT_H */
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- grpc/src/testing/simple_grpc.cpp
+++ grpc/src/testing/simple_grpc.cpp
@@ -15,48 +15,24 @@ using grpc::Status;
 
 using namespace arax;
 
-int main(int argc, char* argv[]){
+int main(int argc, char *argv[])
+{
+    AraxClient client(grpc::CreateChannel("localhost:50051", grpc::InsecureChannelCredentials()));
 
-  AraxClient client(grpc::CreateChannel("localhost:50051", grpc::InsecureChannelCredentials()));
+    // Request buffer
+    const uint64_t buffer = client.client_arax_buffer(5);
 
-  // Request buffer
-  const uint64_t buffer = client.client_arax_buffer(5);
+    // Request accelerator
+    const uint64_t accel1 = client.client_arax_accel_acquire_type(CPU);
 
-  // Request accelerator
-  const uint64_t accel1 = client.client_arax_accel_acquire_type(CPU);
-  client.client_arax_accel_release(accel1);
+    client.client_arax_accel_release(accel1);
 
-  const uint64_t proc = client.client_arax_proc_register("Random");
+    const uint64_t proc = client.client_arax_proc_register("Random");
 
-  client.client_arax_proc_put(proc);
+    client.client_arax_proc_put(proc);
 
-  // This call should get Arax to sleep
-  // client.client_arax_pipe_get_orphan_vaccel(); 
+    std::cout << "ID of buffer: " << buffer << "\n";
+    std::cout << "ID of proc: " << proc << "\n";
 
-  // There is no vaccel with ID 0, this should throw an error
-  client.client_arax_pipe_add_orphan_vaccel(0);
-
-  if(client.client_arax_pipe_have_orphan_vaccels()){
-    std::cout << "-- There are orphan vaccels --\n";
-  }else{
-    std::cout << "-- There are NO orphan vaccels --\n";
-  }
-
-  // Remove accel that does not exist
-  client.client_arax_pipe_remove_orphan_vaccel(0);
-
-  const char* revision = client.client_arax_pipe_revision();
-  std::cout << "Revision: " << revision << std::endl;
-
-  uint64_t proc_counter = client.client_arax_pipe_add_process();
-  std::cout << "Number of active processes (Before adding issuer): " << proc_counter << "\n";
-
-  proc_counter = client.client_arax_pipe_del_process();
-  std::cout << "Number of active processes (Before removing issuer): " << proc_counter << "\n";
-
-
-  std::cout << "ID of buffer: " << buffer << "\n";
-  std::cout << "ID of proc: " << proc << "\n"; 
-
-  return 0;
+    return 0;
 }
